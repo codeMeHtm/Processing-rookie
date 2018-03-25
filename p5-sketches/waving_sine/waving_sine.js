1a553ca2a5b56046c22c7221c9d484ae05586634
Jonathan Richer <36356746+codeMeHtm@users.noreply.github.com>
2018-03-25 14:24:57 -0400

    Update waving_sine.js

--- p5-sketches/waving_sine/waving_sine.js
+++ p5-sketches/waving_sine/waving_sine.js
@@ -6,11 +6,11 @@ let rx;
 function setup() {
   createCanvas(600, 400);
   colorMode(HSB);
-  background(0);
   frameRate(15);
 }
 
 function draw() {
+  background(0);
   // Making the xs and ys for the points
   let x = frameCount * 0.5;
   let y11 = height / 2 + sin(angle) * (height/4);
@@ -24,8 +24,8 @@ function draw() {
   let b21 = map(y21, height / 4 + height / 2, height / 4, 50, 100);
   let b22 = map(y22, height / 4 + height / 2, height / 4, 50, 100);
   increment = TWO_PI / (2*(height / 2));
-  // Making the points.
-
+  // Making the points
+  
   // Sine 1
   stroke(h, 100, b11);
   strokeWeight(5);
@@ -47,16 +47,29 @@ function draw() {
   point(x, y22);
 
   // Progression line
+  /*
   stroke(0, 0, 255);
-  strokeWeight(5);
+  strokeWeight(8);
   line(0, height/4, x, height/4);
   stroke(0, 0, 255);
-  strokeWeight(5);
+  strokeWeight(8);
   line(0, height/4 + height/2, x, height/4 + height/2);
   let centerLine = height/2;
   stroke(0, 0, 255);
-  strokeWeight(5);
+  strokeWeight(7);
   line(0, height/2, x, height/2);
+  */
+  
+  stroke(0, 0, 255);
+  strokeWeight(8);
+  point(x, height/2);
+  stroke(0, 0, 255);
+  strokeWeight(8);
+  point(x, height/4);
+  stroke(0, 0, 255);
+  strokeWeight(8);
+  point(x, height/4 + height/2);
+  
 
   // Recorded colors
   rx = x;
@@ -79,7 +92,7 @@ function draw() {
   if (y21 < centerLine + 1 && y21 > centerLine - 1) centers.push(ctr2);
   for (center of centers) {
     stroke(center.h, 100, center.b);
-    strokeWeight(8);
+    strokeWeight(10);
     point(center.x, height/2);
   }
   angle += increment;
