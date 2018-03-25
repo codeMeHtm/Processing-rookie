0c935d92e435e561412246a384caceda359fa2d8
codeMeHtm <jonathan.rdl@outlook.com>
2018-03-25 19:43:47 -0400

    Adding features to the sine wave

--- p5-sketches/waving_sine/waving_sine.js
+++ p5-sketches/waving_sine/waving_sine.js
@@ -2,99 +2,70 @@ let centers = [];
 let angle = 0;
 let increment;
 let rx;
+let points = [];
+let x = 0;
+let xvel = 2;
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(600, 200);
   colorMode(HSB);
-  frameRate(15);
 }
 
 function draw() {
   background(0);
   // Making the xs and ys for the points
-  let x = frameCount * 0.5;
-  let y11 = height / 2 + sin(angle) * (height/4);
-  let y12 = height / 2 + sin(angle) * -(height/4);
-  let y21 = height / 2 + cos(angle) * (height/4);
-  let y22 = height / 2 + cos(angle) * -(height/4);
+  let y11 = height / 2 + 20 + sin(angle) * 20;
+  let y12 = height / 2 + sin(angle) * 20;
+  let y21 = height / 2 + cos(angle) * 20;
+  let y22 = height / 2 + 20 + cos(angle) * 20;
   // Colors
   let h = map(x, 0, width * 2, 0, 360);
   let b11 = map(y11, height / 4, height / 4 + height / 2, 50, 100);
   let b12 = map(y12, height / 4, height / 4 + height / 2, 50, 100);
   let b21 = map(y21, height / 4 + height / 2, height / 4, 50, 100);
   let b22 = map(y22, height / 4 + height / 2, height / 4, 50, 100);
-  increment = TWO_PI / (2*(height / 2));
+  increment = TWO_PI / 50;
   // Making the points
-  
-  // Sine 1
-  stroke(h, 100, b11);
-  strokeWeight(5);
-  point(x, y11);
 
-  // Sine 2
-  stroke(h, 100, b12);
-  strokeWeight(5);
-  point(x, y12);
-
-  // Cosine 1
-  stroke(h, 100, b21);
-  strokeWeight(5);
-  point(x, y21);
-
-  // Cosine 2
-  stroke(h, 100, b22);
-  strokeWeight(5);
-  point(x, y22);
-
-  // Progression line
-  /*
-  stroke(0, 0, 255);
-  strokeWeight(8);
-  line(0, height/4, x, height/4);
-  stroke(0, 0, 255);
-  strokeWeight(8);
-  line(0, height/4 + height/2, x, height/4 + height/2);
-  let centerLine = height/2;
-  stroke(0, 0, 255);
-  strokeWeight(7);
-  line(0, height/2, x, height/2);
-  */
-  
-  stroke(0, 0, 255);
-  strokeWeight(8);
-  point(x, height/2);
-  stroke(0, 0, 255);
-  strokeWeight(8);
-  point(x, height/4);
-  stroke(0, 0, 255);
-  strokeWeight(8);
-  point(x, height/4 + height/2);
-  
-
-  // Recorded colors
-  rx = x;
-  rb1 = b11;
-  rb2 = b21;
-  rh = h;
-  let ctr1 = {
-    x: rx, 
-    b: rb1, 
-    h: rh
+  let point1 = {
+    x: x,
+    y: y11,
+    b: b11
   };
-  let ctr2 = {
-    x: rx, 
-    b: rb2, 
-    h: rh
+  let point2 = {
+    x: x,
+    y: y12,
+    b: b12
+  };
+  let point3 = {
+    x: x,
+    y: y21,
+    b: b21
+  };
+  let point4 = {
+    x: x,
+    y: y22,
+    b: b22
   };
 
-  // Center dots
-  if (y11 < centerLine + 1 && y11 > centerLine - 1) centers.push(ctr1);
-  if (y21 < centerLine + 1 && y21 > centerLine - 1) centers.push(ctr2);
-  for (center of centers) {
-    stroke(center.h, 100, center.b);
-    strokeWeight(10);
-    point(center.x, height/2);
+  points.push(point1);
+  points.push(point2);
+  // points.push(point3);
+  // points.push(point4);
+
+  for (let i = 0; i < points.length; i++) {
+    stroke(h, 100, points[i].b);
+    strokeWeight(5);
+    point(points[i].x, points[i].y);
   }
+  
   angle += increment;
-  if (frameCount * 0.5 == width) noLoop();
+  if (x >= width || x <= -1) xvel *= -1;
+  if (points.length >= 250) {
+    points.shift();
+    points.shift();
+    points.shift();
+    points.shift();
+  }
+  x += xvel;
 }
