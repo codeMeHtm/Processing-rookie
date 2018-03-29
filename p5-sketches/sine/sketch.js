c65e5bd2963b56433fc2f738b98089a2fee9fbd9
codeMeHtm <jonathan.rdl@outlook.com>
2018-03-29 08:03:08 -0400

    adding a slider to control the framerate

--- p5-sketches/sine/sketch.js
+++ p5-sketches/sine/sketch.js
@@ -8,37 +8,42 @@ let cincrement;
 const points = [];
 let x1 = 10;
 let x1vel = 3;
+let button;
+let clicked = false;
+let slider;
 
 function setup() {
-	createCanvas(600, 400);
+	createCanvas(windowWidth, 400);
+	increment = PI/25;
+	const p = createP("Slide it to set the Frame Rate!");
+	slider = createSlider(20, 60, 30, 1);
+	p.style("font-size", "50px");
+	p.style("color", "#fff")
+	document.body.style.background = "#000";
 }
 
 function draw() {
 	cincrement = TWO_PI/30;
-	increment = PI/20;
+	frameRate(slider.value());
 	
 	background(0);
-	noStroke();
-	fill(255);
-	rect(0, 0, width, height/4);
-	rect(0, height/2 + height/4, width, height/2 + height/4);
 	const circle1 = {
 		x: x,
-		y: height/2 + cos(angle) * 25,
+		y: height/2 + height/4 + cos(angle) * 25,
 		r: 10
 	};
 	const circle2 = {
 		x: x,
-		y: height/2 + cos(angle) * -25,
+		y: height/2 + height/4 + cos(angle) * -25,
 		r: 10
 	};
 	const point1 = {
 		x: x1,
-		y: height/2 + sin(cangle) * 20
+		y: height/2 + height/4 + sin(cangle) * 20
 	};
 	const point2 = {
 		x: x1,
-		y: height/2 + sin(cangle) * -20
+		y: height/2 + height/4 + sin(cangle) * -20
 	};
 
 	points.push(point1);
@@ -54,8 +59,8 @@ function draw() {
 		ellipse(c.x, c.y, c.r, c.r);
 	}
 
-	stroke(255, 0, 255);
-	strokeWeight(8);
+	stroke(0, 255, 0);
+	strokeWeight(10);
 	for (p of points) {
 		point(p.x, p.y);
 	}
@@ -69,16 +74,17 @@ function draw() {
 		points.shift();
 		points.shift();
 	}
+	console.log(angle);
 	angle += increment;
 	cangle += cincrement;
 	x += xvel;
 	x1 += x1vel;
 	if (x >= width - 10) {
 		xvel *= -1;
-		increment = TWO_PI/20;
+		increment = TWO_PI/30;
 	} else if (x <= 0 + 10) {
 		xvel *= -1;
-		increment = PI / 10;
+		increment = PI / 20;
 	}
 	if (x1 >= width - 10 || x1 <= 0 + 10) x1vel *= -1;
 }
