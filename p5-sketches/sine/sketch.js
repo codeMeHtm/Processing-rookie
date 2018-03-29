cb7792b48a8e64780ac20a6bfa2cfc665be52c85
codeMeHtm <jonathan.rdl@outlook.com>
2018-03-28 21:33:05 -0400

    adding another wave

--- p5-sketches/sine/sketch.js
+++ p5-sketches/sine/sketch.js
@@ -1,47 +1,84 @@
-let circles = [];
+const circles = [];
 let angle = 0;
 let increment;
-let x = 0 + 10;
-let xvel = 1;
+let x = 10;
+let xvel = 3;
+let cangle = 0;
+let cincrement;
+const points = [];
+let x1 = 10;
+let x1vel = 3;
 
 function setup() {
 	createCanvas(600, 400);
 }
 
 function draw() {
-	increment = TWO_PI / 50;
+	cincrement = TWO_PI/30;
+	increment = PI/20;
+	
 	background(0);
-	let circle1 = {
+	noStroke();
+	fill(255);
+	rect(0, 0, width, height/4);
+	rect(0, height/2 + height/4, width, height/2 + height/4);
+	const circle1 = {
 		x: x,
 		y: height/2 + cos(angle) * 25,
 		r: 10
 	};
-	let circle2 = {
+	const circle2 = {
 		x: x,
 		y: height/2 + cos(angle) * -25,
 		r: 10
 	};
+	const point1 = {
+		x: x1,
+		y: height/2 + sin(cangle) * 20
+	};
+	const point2 = {
+		x: x1,
+		y: height/2 + sin(cangle) * -20
+	};
+
+	points.push(point1);
+	points.push(point2);
+
 
 	noStroke();
 	fill(255);
 	circles.push(circle1);
 	circles.push(circle2);
-	for (let i = 0; i < circles.length; i++) {
-		if (i % 2 == 0) {
-			fill(255);
-			ellipse(circles[i].x, circles[i].y, circles[i].r, circles[i].r);
-		} else {
-			fill(255, 0, 255);
-			ellipse(circles[i].x, circles[i].y, circles[i].r, circles[i].r);
-		}
+	for (c of circles) {
+		fill(255);
+		ellipse(c.x, c.y, c.r, c.r);
 	}
 
-	if (circles.length >= 152) {
+	stroke(255, 0, 255);
+	strokeWeight(8);
+	for (p of points) {
+		point(p.x, p.y);
+	}
+
+
+	if (circles.length >= 50) {
 		circles.shift();
 		circles.shift();
+	}
+	if (points.length >= 40) {
+		points.shift();
+		points.shift();
 	}
 	angle += increment;
+	cangle += cincrement;
 	x += xvel;
-
-	if (x >= width - 10 || x <= 0 + 10) xvel *= -1;
+	x1 += x1vel;
+	if (x >= width - 10) {
+		xvel *= -1;
+		increment = TWO_PI/20;
+	} else if (x <= 0 + 10) {
+		xvel *= -1;
+		increment = PI / 10;
+	}
+	if (x1 >= width - 10 || x1 <= 0 + 10) x1vel *= -1;
 }
